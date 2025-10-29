d17e1fe8b2dcd1878354062fc4e9f6cb3fb57e7a
fakhrifawwaz <email@fakhri07fawwaz@gmai.com>
2025-10-29 11:11:41 +0700

    tugas 1

---
+++ TugasJumlahKuadrat15.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TugasJumlahKuadrat15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int jumlah = 0;
+            System.out.print("n = " + i + " -> jumlah kuadrat = ");
+
+            for (int j = 1; j <= i; j++) {
+                int kuadrat = j * j;
+                jumlah += kuadrat;
+
+                if (j < i)
+                    System.out.print(kuadrat + " + ");
+                else
+                    System.out.print(kuadrat);
+            }
+
+            System.out.println(" = " + jumlah);
+        }
+
+        sc.close();
+    }
+}
